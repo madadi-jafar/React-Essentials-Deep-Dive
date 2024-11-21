0693a25ae5de23fc7d00306c52c063b7da9e965e
Jafar Madadi <jafar.madadi76@gmail.com>
2024-11-21 14:26:44 +0430

    Update: its an enhanced version of React essentials project which done by Jafar Madadi for my 2025 React Class

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,12 @@
+export default function Tabs({ children, buttons, ButtonContainer="menu" }) {
+    // const ButtonContainer = buttonsContainer;
+
+
+    return (
+        <>
+            <ButtonContainer>{buttons} </ButtonContainer>
+            {children}
+        </>
+
+    );
+}
